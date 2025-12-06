4cb6112accb416b6eb09ab45bc6207f69ebaf130
fengyeyingxiyang <37039196+fengyeyingxiyang@users.noreply.github.com>
2025-12-06 11:31:14 +0800

    Update main.go

--- cmd/grsc/main.go
+++ cmd/grsc/main.go
@@ -26,14 +26,34 @@ func main() {
 		logger.Fatalln(err)
 	}
 	c := client{logger: logger, config: config, socksServer: socksServer}
-	for {
-		err = c.serve()
-		if err != nil {
-			logger.Errorf("serve: %v", err)
-		}
-		logger.Infoln("sleep 5s")
-		time.Sleep(time.Second * 5)
 
+	// 重连间隔表（秒）
+	retryTable := []int{5, 10, 20, 40, 80, 160, 320, 640}
+	retryIndex := 0
+
+	for {
+		success := false
+		err = c.serve(&success)
+		if err != nil {
+			c.logger.Errorf("serve: %v", err)
+
+			// 如果刚刚成功连接过一次，重置 retryIndex
+			if success {
+				retryIndex = 0
+			}
+
+			wait := retryTable[retryIndex]
+			c.logger.Infof("sleep %ds", wait)
+			time.Sleep(time.Duration(wait) * time.Second)
+
+			// 递增重试次数（最多到最后一个 640 秒）
+			if retryIndex < len(retryTable)-1 {
+				retryIndex++
+			}
+		} else {
+			// serve() 正常退出（极少见），重置重连
+			retryIndex = 0
+		}
 	}
 }
 
@@ -43,19 +63,23 @@ type client struct {
 	socksServer *socks5.Server
 }
 
-func (c *client) serve() error {
+// serve 修改为传入 success 指针，用于标记是否成功连接
+func (c *client) serve(success *bool) error {
 	c.logger.Infoln("try connect to server")
 	client, err := reality.NewClient(context.Background(), c.config)
 	if err != nil {
 		return err
 	}
+	*success = true // 成功建立连接
 	c.logger.Infoln("server connected")
 	defer client.Close()
+
 	session, err := yamux.Client(client, nil)
 	if err != nil {
 		return err
 	}
 	defer session.Close()
+
 	for {
 		stream, err := session.Accept()
 		if err != nil {
